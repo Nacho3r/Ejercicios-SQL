90adbb593705371b0d25518bafc759a8c1ca556b
Nacho3r <nacho3r@hotmail.com>
2024-01-20 08:35:12 +0100

    sigo actualizando

--- Eje1.sql
+++ Eje1.sql
@@ -1,20 +1,20 @@
-'2. Insertar un nuevo cliente en la tabla "Clientes" con id=1, nombre="Juan" y
-email="juan@example.com".'
+/*2. Insertar un nuevo cliente en la tabla "Clientes" con id=1, nombre="Juan" y
+email="juan@example.com".*/
 INSERT INTO public.clientes(name, email)
 VALUES ('Juan', 'juan@email.com')
 
-'3. Actualizar el email del cliente con id=1 a "juan@gmail.com"'
+/*3. Actualizar el email del cliente con id=1 a "juan@gmail.com"*/
 UPDATE public.clientes
 SET email = 'juan@gmail.com'
 WHERE id = 1;
 
-'4. Eliminar el cliente con id=1 de la tabla "Clientes"'
+/*4. Eliminar el cliente con id=1 de la tabla "Clientes"*/
 DELETE FROM public.clientes 
 WHERE id = 1;
 
-'5. Crear una tabla llamada "Pedidos" con las columnas: id (entero, clave primaria),
+/*5. Crear una tabla llamada "Pedidos" con las columnas: id (entero, clave primaria),
 cliente_id (entero, clave externa referenciando a la tabla "Clientes"), producto
-(texto) y cantidad (entero).'
+(texto) y cantidad (entero).*/
 CREATE TABLE IF NOT EXISTS Pedidos(
 	id SERIAL PRIMARY KEY,
 	cliente_id INT,
@@ -22,3 +22,21 @@ CREATE TABLE IF NOT EXISTS Pedidos(
 	cantidad INT,
 	CONSTRAINT FK_cliente_id FOREIGN KEY (cliente_id) REFERENCES clientes (id)
 )
+
+/*. Insertar un nuevo pedido en la tabla "Pedidos" con id=1, cliente_id=1,
+producto="Camiseta" y cantidad=2.*/
+INSERT INTO public.pedidos(cliente_id, producto, cantidad)
+VALUES (2, 'Camiseta', 2)
+
+/*Actualizar la cantidad del pedido con id=1 a 3.*/
+UPDATE public.pedidos
+SET cantidad = 3
+WHERE id = 1
+
+/*8. Eliminar el pedido con id=1 de la tabla "Pedidos".*/
+DELETE FROM public.pedidos
+WHERE id = 1
+
+/*. Crear una tabla llamada "Productos" con las columnas: id (entero, clave
+primaria), nombre (texto) y precio (decimal).*/
+
